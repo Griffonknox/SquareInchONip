962db63750bb3dd747740e46a4756017ebdf169a
griffonknox <thompsonscott1180@gmail.com>
2022-07-18 14:39:24 -0500

    pushcommit

---
+++ app/config.py
@@ -0,0 +1,6 @@
+
+class Config(object):
+    SECRET_KEY = 'a5f7f3f3d18eaa317ac1e36a5632a862a98ace1c97dd4ac3'
+    DEBUG = True
+
+
---
+++ run.py
@@ -0,0 +1,13 @@
+"""
+Author: Scott Thompson
+Description: Square Inch O' Nip
+An applicaiton that will calculate the square inch of a nipple.
+Seriously though, its a test to practice restfulAPI's but its wrapped in a little humor.
+"""
+
+from app import create_app
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run()
